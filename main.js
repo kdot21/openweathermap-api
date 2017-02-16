d3a16de3cbc9e6662d5273628a14fddc22fa29d5
Kenneth Mancini <kennethjmancini@gmail.com>
2017-02-16 17:24:54 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+//OpenWeatherMap API Challenge
+var OPEN_WEATHER_MAP_API = "https://circuits-api.generalassemb.ly/8737fcf3-6a39-4548-a324-209d535e59fd?q=";
+var resultElement = $("#result");
+
+function callOpenWeatherMap(city) {
+  if (city.length == 0) {
+    $("#result").html("Please enter a city name into the search field");
+  } else {
+    $.get(OPEN_WEATHER_MAP_API + city, function(searchResult) {      
+      var celsiusTemp = toCelsius(searchResult.main.temp);
+      var stringOutput;
+      stringOutput = "<p>"+ searchResult.name + "<br>";
+      stringOutput += "<p>"+ celsiusTemp + " C" + "<br>";
+      stringOutput +="</p>";
+      
+      resultElement.html(stringOutput);
+    });
+  }
+}
+
+function toCelsius(kelvinTemp) {
+  var temp = Math.round(kelvinTemp - 273.15);
+  return temp;
+}
+
+$("#searchBtn").on("click", function(e) {
+  e.preventDefault();
+  
+  callOpenWeatherMap($("#searchField").val());
+});
